0c8e97c0ec1f5f05b353fadea935a68b0025671e
«Anastasia» <muzalkova.anastasia@gmail.com>
2022-08-16 19:49:34 +0300

    add all

---
+++ extra/Program.cs
@@ -0,0 +1,7 @@
+﻿//Разворот массива
+Console.WriteLine ("Введите число: ");
+string number = (Console.ReadLine ());
+
+char[] arr = number.ToCharArray();
+Array.Reverse (arr);
+Console.WriteLine (arr);
---
+++ task19/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.WriteLine ("Введите число: ");
+string number = (Console.ReadLine ());
+
+char[] arr = number.ToCharArray();
+Array.Reverse (arr);
+Console.WriteLine (arr);
+
+
+
+    if (number[0] == arr[0] & number[1] == arr[1] & number[2] == arr[2] & number[3] == arr[3] & number[4] == arr[4])
+{
+   Console.WriteLine ("да");
+}
+ 
+    else
+{
+     Console.WriteLine ("нет");
+}
